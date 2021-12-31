1fff76e821330a6e3d5e7c82261872befe477672
KIM DAEYEOP <secrisa11@gmail.com>
2022-01-01 08:05:15 +0900

    Create a session

--- image_upload/server/models/User.js
+++ image_upload/server/models/User.js
@@ -5,6 +5,11 @@ const UserSchema = new mongoose.Schema(
     name: { type: String, require: true },
     username: { type: String, required: true, unique: true },
     hashedPassword: { type: String, required: true },
+    sessions: [
+      {
+        createdAt: { type: Date, required: true },
+      },
+    ],
   },
   { timestamps: true }
 );
--- image_upload/server/routes/userRouter.js
+++ image_upload/server/routes/userRouter.js
@@ -10,12 +10,18 @@ userRouter.post("/register", async (req, res) => {
     if (req.body.username.length < 3)
       throw new Error("username은 3자 이상으로 해주세요.");
     const hashedPassword = await hash(req.body.password, 10);
-    await new User({
+    const user = await new User({
       name: req.body.name,
       username: req.body.username,
       hashedPassword,
+      sessions: [{ createdAt: new Date() }],
     }).save();
-    res.json({ message: "user registered" });
+    const session = user.sessions[0];
+    res.json({
+      message: "user registered",
+      sessionId: session._id,
+      name: user.name,
+    });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
@@ -26,7 +32,14 @@ userRouter.post("/login", async (req, res) => {
     const user = await User.findOne({ username: req.body.username });
     const isValid = await compare(req.body.password, user.hashedPassword);
     if (!isValid) throw new Error("입력하신 정보가 올바르지 않습니다.");
-    res.json({ message: "user validated" });
+    user.sessions.push({ createdAt: new Date() });
+    const session = user.sessions[user.sessions.length - 1];
+    await user.save();
+    res.json({
+      message: "user validated",
+      sessionId: session._id,
+      name: user.name,
+    });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }

1c88b402b525ab3da95127a3f0e9e0f17400ab72
KIM DAEYEOP <secrisa11@gmail.com>
2022-01-01 05:35:15 +0900

    Setting up mongodb atlas and connecting with Node.js

--- image_upload/server/server.js
+++ image_upload/server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const { v4: uuid } = require("uuid");
 const mime = require("mime-types");
+const mongoose = require("mongoose");
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => callback(null, "./uploads"),
@@ -22,12 +23,18 @@ const upload = multer({
 const app = express();
 const PORT = 5000;
 
-app.use("/uploads", express.static("uploads"));
-
-app.post("/upload", upload.single("image"), (req, res) => {
-  res.json(req.file);
-});
-
-app.listen(PORT, () => {
-  console.log(`Express server listening on PORT ${PORT}`);
-});
+mongoose
+  .connect(
+    "mongodb+srv://admin:FrZVXyUd5qGAx3ds@cluster0.ygatk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("MongoDB Connected.");
+    app.use("/uploads", express.static("uploads"));
+    app.post("/upload", upload.single("image"), (req, res) => {
+      res.json(req.file);
+    });
+    app.listen(PORT, () => {
+      console.log(`Express server listening on PORT ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));

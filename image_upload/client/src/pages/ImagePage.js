3bfd7ebe635157be80ce1d65807113a82d3042d3
KIM DAEYEOP <secrisa11@gmail.com>
2022-01-03 02:27:36 +0900

    Deleting images

--- image_upload/client/src/pages/ImagePage.js
+++ image_upload/client/src/pages/ImagePage.js
@@ -3,8 +3,11 @@ import { useParams } from "react-router-dom";
 import { ImageContext } from "../context/ImageContext";
 import { AuthContext } from "../context/AuthContext";
 import axios from "axios";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const ImagePage = () => {
+  const navigate = useNavigate();
   const { imageId } = useParams();
   const { images, myImages, setImages, setMyImages } = useContext(ImageContext);
   const [me] = useContext(AuthContext);
@@ -35,6 +38,19 @@ const ImagePage = () => {
     setHasLiked(!hasLiked);
   };
 
+  const deleteHandler = async () => {
+    try {
+      if (!window.confirm("정말 해당 이미지를 삭제하시겠습니까?")) return;
+      const result = await axios.delete(`/images/${imageId}`);
+      toast.success(result.data.message);
+      setImages(images.filter((image) => image._id !== imageId));
+      setMyImages(myImages.filter((image) => image._id !== imageId));
+      navigate("/");
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
+
   return (
     <div>
       <h3>Image Page - {imageId}</h3>
@@ -44,6 +60,14 @@ const ImagePage = () => {
         src={`http://localhost:5000/uploads/${image.key}`}
       />
       <span>좋아요 {image.likes.length}</span>
+      {me && image.user._id === me.userId && (
+        <button
+          style={{ float: "right", marginLeft: 10 }}
+          onClick={deleteHandler}
+        >
+          삭제
+        </button>
+      )}
       <button onClick={onSubmit} style={{ float: "right" }}>
         {hasLiked ? "좋아요 취소" : "좋아요"}
       </button>
